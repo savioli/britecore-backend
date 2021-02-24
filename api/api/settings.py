9f0367d0fa68c1897b185944f9053aefae095633
André Savioli <andre_savioli@hotmail.com>
2021-02-24 00:23:10 +0000

    Implemented the database connection to PostgreSQL on Django

--- api/api/settings.py
+++ api/api/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'britecore',
+        'USER': 'postgres',
+        'PASSWORD': 'britecore',
+        'HOST': 'postgres',
+        'PORT': '5432',
     }
 }
 
