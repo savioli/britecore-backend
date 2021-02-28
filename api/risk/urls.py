80b674dcdbf852b7a5847179dbcc78af06cb14ef
André Savioli <andre_savioli@hotmail.com>
2021-02-28 16:30:18 +0000

    Updated the path of the endpoint to retrieve a single Risk

--- api/risk/urls.py
+++ api/risk/urls.py
@@ -3,5 +3,5 @@ from risk.views import RiskFormViewSet
 from django.conf.urls import url
 
 urlpatterns = [
-    url(r"^risks/(?P<pk>\d+)/form/$", RiskFormViewSet.as_view({"get": "retrieve"})),
+    url(r"^risks/(?P<pk>\d+)$", RiskFormViewSet.as_view({"get": "retrieve"})),
 ]
