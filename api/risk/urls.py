68628fc65ec782f2645989f4df720b1e8c1bd5e9
André Savioli <andre_savioli@hotmail.com>
2021-02-28 18:28:38 +0000

    Adjustment of routes considering the precedence

--- api/risk/urls.py
+++ api/risk/urls.py
@@ -3,6 +3,6 @@ from risk.views import RiskFormViewSet, RiskViewSet
 from django.conf.urls import url
 
 urlpatterns = [
-    url(r"^risks", RiskViewSet.as_view({"get": "list"})),
     url(r"^risks/(?P<pk>\d+)$", RiskFormViewSet.as_view({"get": "retrieve"})),
+    url(r"^risks", RiskViewSet.as_view({"get": "list"})),
 ]
