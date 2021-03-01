191cce6caebee1a1dbaffe02ee7db24b89b112a7
André Savioli <andre_savioli@hotmail.com>
2021-03-01 21:02:56 +0000

    Added the attribute description to the model RiskFieldEnumOption

--- api/risk/models.py
+++ api/risk/models.py
@@ -60,6 +60,7 @@ class RiskFieldEnumOption(models.Model):
     """A Model that defines the representation of a RiskFieldEnumOption"""
 
     name = models.CharField(max_length=128)
+    description = models.CharField(max_length=128)
 
     def __str__(self):
         return self.name
