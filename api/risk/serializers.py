9676822e792c445165134254f14bd7c60e058ab3
André Savioli <andre_savioli@hotmail.com>
2021-03-01 05:43:18 +0000

    Implemented the RiskRiskFieldSerializer that generates the information about the RiskRiskField model that intermediates the Risk and RiskField models

--- api/risk/serializers.py
+++ api/risk/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework.serializers import ModelSerializer
-from risk.models import Risk, RiskCategory, RiskField, RiskFieldType
+from risk.models import Risk, RiskCategory, RiskField, RiskFieldType, RiskRiskField
 
 
 class RiskCategorySerializer(ModelSerializer):
@@ -34,13 +34,41 @@ class RiskFieldSerializer(ModelSerializer):
         model = RiskField
 
 
+class RiskRiskFieldSerializer(ModelSerializer):
+    """A Serializer that generates the information 
+    about the model RiskRiskField that intermediates
+    the model Risk and the model RiskField
+    """
+
+    def to_representation(self, instance):
+
+        riskField = RiskField.objects.get(pk=instance.risk_field_id)
+        riskType = riskField.risk_field_type
+
+        riskField = RiskFieldSerializer(riskField).data
+
+        riskField.update({"required": instance.required, "type": riskType.code})
+
+        riskType = riskField.pop("risk_field_type")
+
+        return riskField
+
+    class Meta:
+        fields = ["id", "name"]
+
+        model = RiskRiskField
+
+
 class RiskFormSerializer(ModelSerializer):
     """A Serializer that generates all the information about
     the Risk and about the RiskField's
     """
 
     risk_category = RiskCategorySerializer()
-    risk_fields = RiskFieldSerializer(many=True)
+
+    risk_fields = RiskRiskFieldSerializer(
+        source="risk_to_field", many=True, read_only=True
+    )
 
     class Meta:
         model = Risk
