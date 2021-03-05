5ea1f2734c2fa16b298f7c530ce7fe27b191c843
André Savioli <andre_savioli@hotmail.com>
2021-03-05 16:55:03 +0000

    Implemented the function create_a_risk_category to help the creation of RiskCategory objects during the tests

--- api/risk/tests.py
+++ api/risk/tests.py
@@ -1,7 +1,20 @@
 from rest_framework.test import APITestCase
+from risk.models import RiskCategory
 
 
 class RiskAPITestCase(APITestCase):
     """Test class for the Risk API"""
 
     fixtures = ["risk/fixtures/risk_field_type.json"]
+
+    def create_a_risk_category(self, code="test_category"):
+        """A function to help the creation of RiskCategory objects during the tests"""
+
+        risk_category = RiskCategory(
+            name="Test Category",
+            description="Test category description",
+            code=code,
+        )
+        risk_category.save()
+
+        return risk_category
