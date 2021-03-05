f5a898cc1d06c929fa5eb26ef69faa54fca72365
André Savioli <andre_savioli@hotmail.com>
2021-03-05 16:58:10 +0000

    Implemented the function create_a_risk_field_by_field_type to help the creation of RiskField objects during the tests

--- api/risk/tests.py
+++ api/risk/tests.py
@@ -1,5 +1,5 @@
 from rest_framework.test import APITestCase
-from risk.models import Risk, RiskCategory
+from risk.models import Risk, RiskCategory, RiskField, RiskFieldType
 
 
 class RiskAPITestCase(APITestCase):
@@ -26,3 +26,37 @@ class RiskAPITestCase(APITestCase):
         risk.save()
 
         return risk
+
+    def create_a_risk_field_by_field_type(self, risk_field_type):
+        """A function to help the creation of RiskField objects based on the type during the tests"""
+
+        if risk_field_type not in [
+            RiskFieldType.TEXT,
+            RiskFieldType.NUMBER,
+            RiskFieldType.DATE,
+            RiskFieldType.ENUM,
+        ]:
+            raise ValueError("Invalid FieldType")
+
+        text_risk_field_type = RiskFieldType.objects.filter(code=risk_field_type).get()
+
+        if RiskFieldType.TEXT == risk_field_type:
+            risk_field_type_name = "Text"
+
+        elif RiskFieldType.NUMBER == risk_field_type:
+            risk_field_type_name = "Number"
+
+        elif RiskFieldType.DATE == risk_field_type:
+            risk_field_type_name = "Date"
+
+        elif RiskFieldType.ENUM == risk_field_type:
+            risk_field_type_name = "Enum"
+
+        text_risk_field = RiskField(
+            name="Test " + risk_field_type_name + " Risk Field",
+            risk_field_type=text_risk_field_type,
+        )
+
+        text_risk_field.save()
+
+        return text_risk_field
