1bdbb1f9c9f959a3a942a74192cefd0a81cb048b
André Savioli <andre_savioli@hotmail.com>
2021-03-05 17:12:30 +0000

    Implemented the test test_if_a_date_field_present_in_the_fields_attribute_of_risk_has_the_correct_type_attribute

--- api/risk/tests.py
+++ api/risk/tests.py
@@ -345,3 +345,26 @@ class RiskAPITestCase(APITestCase):
             required_attribute = None
 
         self.assertIsNotNone(required_attribute)
+
+    def test_if_a_date_field_present_in_the_fields_attribute_of_risk_has_the_correct_type_attribute(
+        self,
+    ):
+        """Checks the type of the date field attribute"""
+
+        risk_category = self.create_a_risk_category()
+        risk = self.create_a_risk(risk_category)
+
+        date_risk_field = self.create_a_risk_field_by_field_type(RiskFieldType.DATE)
+
+        risk.risk_fields.add(date_risk_field)
+
+        risk.save()
+
+        response = self.client.get("http://localhost:8000/api/v1/risks/" + str(risk.pk))
+
+        try:
+            required_attribute = response.data["fields"][0]["type"]
+        except Exception:
+            required_attribute = None
+
+        self.assertEqual(required_attribute, RiskFieldType.DATE)
